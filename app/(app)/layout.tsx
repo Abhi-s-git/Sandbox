5acc89fa297282034af236de8d0b6c348885859d
Abhi <abhi.s.09.2005@gmail.com>
2026-09-11 15:11:22 +0530

    sidebar added

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,22 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+export default function AppLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <SidebarTrigger />
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,18 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+} from "@/components/ui/sidebar"
+
+function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent />
+      <SidebarFooter />
+    </Sidebar>
+  )
+}
+
+export { AppSidebar }
