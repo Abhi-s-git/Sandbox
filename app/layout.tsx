f68e56b67334c824ef0d3966fc99d64f5a15892c
Abhi <abhi.s.09.2005@gmail.com>
2026-09-11 14:56:57 +0530

    added authentication tool

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { shadcn } from "@clerk/ui/themes"
 import { Geist, Geist_Mono } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
-import {Metadata} from "next";
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+import { cn } from "@/lib/utils"
+import { Metadata } from "next"
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -17,11 +19,10 @@ export const metadata: Metadata = {
     template: "%s . Sandbox",
   },
   description:
-   "Describe a game and watch it come to life. Sandbox is an agentic three.js game builder that plans the scene, writes  the code, and streams playable worlds from plain english.",
-  icons:{
+    "Describe a game and watch it come to life. Sandbox is an agentic three.js game builder that plans the scene, writes  the code, and streams playable worlds from plain english.",
+  icons: {
     icon: "/logo.svg",
   },
-  
 }
 
 export default function RootLayout({
@@ -33,10 +34,17 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        geist.variable
+      )}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ClerkProvider appearance={{ theme: shadcn }}>
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image"
+import { UserButton } from "@clerk/nextjs"
+import { OrganizationSwitcher} from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs/server"
 import {
   Empty,
   EmptyDescription,
@@ -7,25 +10,31 @@ import {
   EmptyTitle,
 } from "@/components/ui/empty"
 
-export default function Page() {
+export default async function Page() {
+  await auth.protect()
+
   return (
-    <Empty className="min-h-dvh">
-      <EmptyHeader>
-        <EmptyMedia>
-          <Image
-            src="/logo.svg"
-            alt="Logo"  
-            width={48}
-            height={48}
-            priority
-          />
-        </EmptyMedia>
-        <EmptyTitle className="text-2xl">What should we build today?</EmptyTitle>
-        <EmptyDescription>
-          Build your own racers, shooters, puzzles and whole worlds using your
-          own words. If you can describe it, you can play it.
-        </EmptyDescription>
-      </EmptyHeader>
-    </Empty>
+    <>
+      <Empty className="min-h-dvh">
+        <EmptyHeader>
+          <EmptyMedia>
+            <Image
+              src="/logo.svg"
+              alt="Logo"  
+              width={48}
+              height={48}
+              priority
+            />
+          </EmptyMedia>
+          <EmptyTitle className="text-2xl">What should we build today?</EmptyTitle>
+          <EmptyDescription>
+            Build your own racers, shooters, puzzles and whole worlds using your
+            own words. If you can describe it, you can play it.
+          </EmptyDescription>
+        </EmptyHeader>
+      </Empty>
+      <UserButton />
+      <OrganizationSwitcher></OrganizationSwitcher>
+    </>
   )
 }
